001b7b330ba2ff679b6a83495809078dd7cc7e21
CApp-chat <78289417+CApp-chat@users.noreply.github.com>
2021-03-09 19:55:30 +0530

    Update loader.js

--- main/loader.js
+++ main/loader.js
@@ -1,1 +1,11 @@
 // javascript starts
+makenews = (json) =>{
+var read = localStorage[json.newsId];
+var validate= (findDays(json.date) < 7) ? true:false;
+var header = json.header;
+var summary = json.s;
+var imgs = json.imgs;
+for(img in Object.keys(imgs)){
+let currentImg = Object.values(imgs)[img],src = currentImg.url,alt = currentImg.alt,pos = currentImg.pos,left = pos.left,top = pos.top,
+}
+}
